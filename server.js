40c12b8aa626995def6037d63c35108e6aafff84
Claudio Bianco <claudiombianco@gmail.com>
2021-06-21 12:56:37 -0300

    new version 6

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello, World! version 5')
+    res.end('Hello, World! version 6')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
