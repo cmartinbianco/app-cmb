f1b9a486837afef3b17c2ace35efe4d1601086d2
Claudio Bianco <claudiombianco@gmail.com>
2021-06-21 13:17:09 -0300

    new version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(res,req){
-    res.send("Hello World!")
+    res.send("Hello World")
 })
 
 app.listen(process.env.PORT || 5000)
