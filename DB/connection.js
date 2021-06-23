c16c36cea516c1b0fb6d46d39109e3f71e1af5cd
Claudio Martin Bianco <39170327+claudiomartinbianco@users.noreply.github.com>
2021-06-23 15:30:09 -0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const URI = "mongodb+srv://mongodbUser:Cl@udi01971@cluster0.ggfky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const URI = ${{ secrets.mongodb }};
+const URI = process.env.mongodb;
 
 const connectDB = async() => {
     await mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true });
